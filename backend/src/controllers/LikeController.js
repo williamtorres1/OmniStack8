f9ddf1ed8f8348e523d2b41f4710114061363a15
William Torres <williamtorres074@gmail.com>
2020-02-19 19:28:26 -0300

    frontend finalized

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,26 +1,28 @@
-const Dev = require('../models/Dev');
+const Dev = require('../models/Dev')
 
 module.exports = {
     async store(req, res){
 
-        const { devId } = req.params;
-        const { user } = req.headers;
+        const { devId } = req.params
+        const { user } = req.headers
 
-        const loggedDev = await Dev.findById(user);
-        const targetDev = await Dev.findById(devId);
+        const loggedDev = await Dev.findById(user)
+        const targetDev = await Dev.findById(devId)
 
         if(!targetDev){
             return res.status(400).json({ error: 'Dev not exist'})
         }
 
-        if(targetDev.likes.includes(loggedDev._id)){
-            console.log('Deu match!');
-        }
-
-        loggedDev.likes.push(targetDev._id);
+        loggedDev.likes.push(targetDev._id)
 
         await loggedDev.save()
 
-        return res.json(loggedDev);
+        console.log(`User ${loggedDev.user} liked ${targetDev.user}`)
+
+        if(targetDev.likes.includes(loggedDev._id)){
+            console.log('Deu match!')
+        }
+
+        return res.json(loggedDev)
     }
 }
