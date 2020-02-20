cee2cc0f42f61ae54a52c6f94f3d869234e1eca1
William Torres <williamtorres074@gmail.com>
2020-02-20 00:13:03 -0300

    login page added

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Login from './src/pages/Login'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#89c980',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1, 
+    backgroundColor:'#89c980', 
+    justifyContent: 'center', //estilização verticalmente
+    alignItems: 'center', //estilização horizontalmente
   },
-});
+  text: {
+    fontWeight:'bold',
+    color:'#FFF',
+  }
+})
